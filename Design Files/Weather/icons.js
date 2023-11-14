8c86d0bc9a005eac8e539f2de8a208f725cf72be
MrRedBeard <britton@x2labs.com>
2023-11-13 22:12:58 -0600

    WIP
    All weather icons updated to be more unified #8
    
    #8 WIP towards unifying weather icons

---
+++ Design Files/Weather/icons.js
@@ -0,0 +1,154 @@
+const translation = [
+    {
+        img: ['hz'],
+        icon: 'pfi-wx-hazy'
+    },
+    {
+        img: ['bkn','nbkn'],
+        icon: 'pfi-wx-cloudy'
+    },
+    {
+        img: ['blizzard','nblizzard'],
+        icon: 'pfi-wx-blizzard'
+    },
+    {
+        img: ['cold','ncold'],
+        icon: 'pfi-wx-cold'
+    },
+    {
+        img: ['du','ndu'],
+        icon: 'pfi-wx-dust-storm'
+    },
+    {
+        img: ['fc','nfc'],
+        icon: 'pfi-wx-tornado'
+    },
+    {
+        img: ['few','nfew'],
+        icon: 'pfi-wx-cloud'
+    },
+    {
+        img: ['fg','nfg'],
+        icon: 'pfi-wx-fog'
+    },
+    {
+        img: ['fu','nfu'],
+        icon: 'pfi-wx-smog'
+    },
+    {
+        img: ['fzra','nfzra'],
+        icon: 'pfi-wx-freezing-rain'
+    },
+    {
+        img: ['fzra_sn','nfzra_sn'],
+        icon: 'pfi-wx-freezing-rain'
+    },
+    {
+        img: ['hi_shwrs','hi_nshwrs'],
+        icon: 'pfi-wx-showers'
+    },
+    {
+        img: ['hi_tsra','hi_ntsra'],
+        icon: 'pfi-wx-thunderstorm'
+    },
+    {
+        img: ['hot'],
+        icon: 'pfi-wx-thermometer-100'
+    },
+    {
+        img: ['hur_warn'],
+        icon: 'pfi-wx-hurricane-warn'
+    },
+    {
+        img: ['hur_watch'],
+        icon: 'pfi-wx-hurricane-warn'
+    },
+    {
+        img: ['ip','nip'],
+        icon: 'pfi-wx-sleet'
+    },
+    {
+        img: ['minus_ra'],
+        icon: 'pfi-wx-rain'
+    },
+    {
+        img: ['ra','nra'],
+        icon: 'pfi-wx-rain'
+    },
+    {
+        img: ['ra_fzra','nra_fzra'],
+        icon: 'pfi-wx-freezing-rain'
+    },
+    {
+        img: ['ra_sn','nra_sn'],
+        icon: 'pfi-wx-rain-snow-mix'
+    },
+    {
+        img: ['ovc','novc'],
+        icon: 'pfi-wx-cloudy'
+    },
+    {
+        img: ['raip','nraip'],
+        icon: 'pfi-wx-freezing-rain'
+    },
+    {
+        img: ['sct','nsct'],
+        icon: 'pfi-wx-bkn-sunny-cloudy'
+    },
+    {
+        img: ['scttsra','nscttsra'],
+        icon: 'pfi-wx-thunderstorm'
+    },
+    {
+        img: ['shra','nshra'],
+        icon: 'pfi-wx-showers'
+    },
+    {
+        img: ['skc'],
+        icon: ['pfi-wx-sunny']
+    },
+    {
+        img: ['nskc'],
+        icon: ['pf-wx-starry']
+    },
+    {
+        img: ['sn','nsn'],
+        icon: 'pfi-wx-snow'
+    },
+    {
+        img: ['snip','nsnip'],
+        icon: 'pfi-wx-rain-snow-mix'
+    },
+    {
+        img: ['tor','ntor'],
+        icon: 'pfi-wx-tornado'
+    },
+    {
+        img: ['tsra','ntsra'],
+        icon: 'pfi-wx-thunderstorm'
+    },
+    {
+        img: ['wind_bkn','nwind_bkn'],
+        icon: 'pfi-wx-wind'
+    },
+    {
+        img: ['wind_few','nwind_few'],
+        icon: 'pfi-wx-wind'
+    },
+    {
+        img: ['wind_ovc','nwind_ovc'],
+        icon: 'pfi-wx-wind'
+    },
+    {
+        img: ['wind_sct','nwind_sct'],
+        icon: 'pfi-wx-wind'
+    },
+    {
+        img: ['wind_skc','nwind_skc'],
+        icon: 'pfi-wx-wind'
+    },
+    {
+        img: ['ts_nowarn','ts_warn','ts_watch'],
+        icon: 'ppfi-wx-thunderstorm'
+    }    
+];
